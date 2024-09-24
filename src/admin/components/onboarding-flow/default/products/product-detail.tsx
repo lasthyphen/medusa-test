73042d350019e709fcaef09fb06c22fac94435fc
Dijets <davidhill0173@gmail.com>
2024-09-24 17:52:24 +0100

    Update product-detail.tsx

--- src/admin/components/onboarding-flow/default/products/product-detail.tsx
+++ src/admin/components/onboarding-flow/default/products/product-detail.tsx
@@ -16,7 +16,7 @@ const ProductDetailDefault = ({ onNext, isComplete, data }: StepContentProps) =>
   const api_key = useMemo(() => keys?.[0]?.id || "", [keys])
   const backendUrl = process.env.MEDUSA_BACKEND_URL === "/" || process.env.MEDUSA_ADMIN_BACKEND_URL === "/" ? 
     location.origin :
-    process.env.MEDUSA_BACKEND_URL || process.env.MEDUSA_ADMIN_BACKEND_URL || "https://monkfish-app-7s3zt.ondigitalocean.app"
+    process.env.MEDUSA_BACKEND_URL || process.env.MEDUSA_ADMIN_BACKEND_URL || "http://localhost:9000"
 
   useEffect(() => {
     if (!isLoading && !keys?.length) {
